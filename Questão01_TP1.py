53d70643301e599a3e16a132bf23838127f2d829
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-10-14 13:39:54 -0300

    Update Questão01_TP1.py

--- Questão01_TP1.py
+++ Questão01_TP1.py
@@ -1,11 +1,22 @@
-def soma():
+def line():
+    print("=" * 40)
+    
+
+def hello():
+    line()
+    print("Olá, seja bem-vindo ao programa que soma os números ímpares em um intervalo proposto.")
+
+def sum():
+    line()
     num = int(input("Digite um número: "))
     s = 0
     for a in range(1, num, 2):
         if a % 2 != 0:
             s += a
     print("A soma dos números ímpares entre 1 e", num, "é", s)
-    print('FIM')
-
-
-soma()
+    
+hello()
+sum()
+line()
+print('FIM')
+line()
