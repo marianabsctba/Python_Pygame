d4dce26e0cc8ef8b199ee8857f9ad9eb6eb220a7
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-10-20 21:26:26 -0300

    Update Questão08_TP1.py

--- Questão08_TP1.py
+++ Questão08_TP1.py
@@ -1,19 +1,22 @@
 def verificar(a, b, c):
+    #tupla
+    t = (a, b, c)
     # ver se é triângulo
-    if (a < b + c) and (b < a + c) and (c < a + b):
+    if t[0] + t[1] > t[2] and t[1] + t[2] > t[0] and t[0] + t[2] > t[1]:
         print("Pode formar um triângulo!")   
         # qual tipo de triângulo
-        if (a == b == c):
+        if t[0] == t[1] == t[2]:
             print("Triângulo equilátero!")
-        elif (a != b != c != a):
+        elif t[0] != t[1] != t[2] != t[0]:
             print("Triângulo escaleno!")
         else:
             print("Triângulo isósceles!")
     else:
         print("Não pode formar um triângulo!")  
-        
+
   
-a  =  float ( input ( "Digite o lado a: \ n " ))
-b  =  float ( input ( "Digite o lado b: \ n " ))
-c  =  float ( input ( "Digite o lado c: \ n " ))
+a  =  float (input("Digite o lado a: " ))
+b  =  float (input("Digite o lado b: " ))
+c  =  float (input("Digite o lado c: " ))
+
 verificar(a, b, c)
