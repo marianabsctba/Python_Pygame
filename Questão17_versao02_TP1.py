a469ba9991f3cf2be44bb8aa4047e2bef88153b6
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-10-21 17:16:33 -0300

    Update Questão17_versao02_TP1.py

--- Questão17_versao02_TP1.py
+++ Questão17_versao02_TP1.py
@@ -1,12 +1,10 @@
-def girar():
-    string_usuario = input("Digite uma palavra: ")
-    x = int(input("Quantas vezes você quer girar a palavra selecionada? "))
-    
-    print("Obs.: O programa só irá girar o equivalente ao correspondente número de letras contidas na palavra.")
-    
+def girar(string_usuario, girar):     
     for letra in range(0,x):
         string_usuario = string_usuario[-1] + string_usuario[:len(string_usuario)-1]
         print(string_usuario[-1] + string_usuario[:len(string_usuario)-1])
-    
+
+
+string_usuario = input("Digite uma palavra: ")
+x = int(input("Quantas vezes você quer girar a palavra selecionada? "))    
        
-girar()
+girar(string_usuario, girar)
