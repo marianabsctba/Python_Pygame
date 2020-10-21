0b655908eb2e5f19681fccdae82e38a5e9b6c70a
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2020-10-21 18:02:45 -0300

    Update Questão01_TP1.py

--- Questão01_TP1.py
+++ Questão01_TP1.py
@@ -10,7 +10,7 @@ def sum():
     line()
     num = int(input("Digite um número: "))
     s = 0
-    for a in range(1, num, 2):
+    for a in range(1, num+1, 2):
         if a % 2 != 0:
             s += a
     print("A soma dos números ímpares entre 1 e", num, "é", s)
